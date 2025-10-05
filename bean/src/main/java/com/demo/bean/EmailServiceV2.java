c01641e66c185eb8eec65fd731f0d8544303c4e3
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-10-05 12:04:09 +0700

    update

--- bean/src/main/java/com/demo/bean/EmailServiceV2.java
+++ bean/src/main/java/com/demo/bean/EmailServiceV2.java
@@ -2,7 +2,6 @@ package com.demo.bean;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class EmailServiceV2 {
     public void sendEmail(String to, String message) {
         System.out.println("Gửi email đến: " + to+ " từ EmailServiceV2");
